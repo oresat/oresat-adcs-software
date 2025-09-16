Bugs fixed. Now rotating all error quaternions into body frame for all torque calculations and RW outputs. Also circumvents need to rotate gain matrix into current reference frame. Target calculations still happen in body frame. Added super simple low pass filter to torque outputs to reduce wheel jerk.

simulation/ADCS_Simulation_Basilisk.py
@@ -288,35 +288,35 @@ if __name__ == "__main__":
     Jzy = Jyz
     Jzz = 0.00651814
     
-    # J = np.array([[Jxx, Jxy, Jxz], # satellite inertia matrix
-    #               [Jyx, Jyy, Jyz], 
-    #               [Jzx, Jzy, Jzz]])
-    J = np.array([[Jxx, 0, 0], # satellite inertia matrix
-                  [0, Jxx, 0], 
-                  [0, 0, Jxx]])
+    J = np.array([[Jxx, Jxy, Jxz], # satellite inertia matrix
+                  [Jyx, Jyy, Jyz], 
+                  [Jzx, Jzy, Jzz]])
+    # J = np.array([[Jxx, 0, 0], # satellite inertia matrix
+    #               [0, Jxx, 0], 
+    #               [0, 0, Jxx]])
     
     mass = 3.05353136 # satellite mass [kg]
                   
     viz_filename = None # sim visualization savename
     print_states = False
     
-    sim_time = 50
+    sim_time = 100
     dynamics_update_time = 0.01
     fsw_update_time = 0.1
     
     # initial satellite states
-    init_rot_axis = [0, 1, 0]# this vector cannot be all zeros or quat.axis_angle_to_quaternion will return nan! 
-    init_rot_angle = 0
+    init_rot_axis = [0, 1, 1]# this vector cannot be all zeros or quat.axis_angle_to_quaternion will return nan! 
+    init_rot_angle = 110
     temp = quat.axis_angle_to_quaternion(init_rot_axis, init_rot_angle)
-    omega_init_rpm = np.array([0.0, 0.0, 0.0])  # initial spin velocties [RPM]
+    omega_init_rpm = np.array([1.0, 2.0, 0.3])  # initial spin velocties [RPM]
     omega_init_rad = omega_init_rpm * 2*np.pi/60  # convert RPM to rad/s
     
     # command rotations relative to initial orientation
-    sat_rot_axis = [1, 0, 0]
-    sat_rot_angle = 90
+    sat_rot_axis = [1, 1.5, 0]
+    sat_rot_angle = 165
     
     # Select the spacecraft pointing reference (which axis/sensor defines boresight):
     # Modes are ST (Star Tracker, +x on body), SC (Selfie Camera, +z on body), and CFC (Cirrus Flux Camera, -z on body)  
-    pointing_reference = "SC"
+    pointing_reference = "ST"
     
     sim_main(sim_time, J, mass, dynamics_update_time, fsw_update_time, viz_filename, init_rot_axis, init_rot_angle, omega_init_rad, sat_rot_axis, sat_rot_angle, pointing_reference, print_states) # call and run simulation

simulation/FlightSoftwareModule.py
@@ -52,8 +52,11 @@ class FlightSoftware(sysModel.SysModel):
         self.q_plusZ_rot = quat.axis_angle_to_quaternion([0,1,0], -90) # rotate star tracker output to +z side of satellite for Selfie Camera from Star Tracker orientation on +x side of satellite
         self.q_minusZ_rot = quat.axis_angle_to_quaternion([0,1,0], 90) # rotate star tracker output to -z side of satellite for Cirrus Flux Camera from Star Tracker orientation on +x side of satellite
         
-        self.u_prev = np.zeros(4)
-        self.torque_rate_lim = .01
+        # Quaternion change-of-basis rotations (comments in parentheses use R to denote a standard right-multiply rotation matrix notation)
+        self.B2ST = quat.axis_angle_to_quaternion([0,1,0], 90) # body to star tracker rotation (nominal right-multiply math notation would therefore be R_st_b)
+        self.ST2B = quat.quat_conjugate(self.B2ST) # star tracker to body rotation (nominal right-multiply math notation would therefore be R_b_st)
+        self.B2CFC = quat.axis_angle_to_quaternion([0,1,0], 180) # body to Cirrus Flux Camera rotation (nominal right-multiply math notation would therefore be R_cfc_b)
+        self.CFC2B = quat.quat_conjugate(self.B2CFC) # Cirrus Flux Camera to body rotation (nominal right-multiply math notation would therefore be R_b_cfc)
 
     def Reset(self, currentTimeNanos):
         print(f"({self.ModelTag}) Reset called at {currentTimeNanos * macros.NANO2SEC:.2f} s")
@@ -78,19 +81,20 @@ class FlightSoftware(sysModel.SysModel):
             self.imuMsg = self.imuMsgIn()
             omega = self.imuMsg.AngVelPlatform
             
-            # convert rates to the same frame that the error quaternion is generated in
-            if self.pointing == "ST":
-                omega = quat.rotate_vec_by_quat(omega, quat.axis_angle_to_quaternion([0,1,0], -90))
-            elif self.pointing == "SC":
-                pass # do nothing, as wheels are already defined in correct frame
-            elif self.pointing == "CFC":
-                omega = quat.rotate_vec_by_quat(omega, quat.axis_angle_to_quaternion([0,1,0], 180))
-                
         if self.rwSpeedMsgIn.isWritten():
             self.rwSpeedMsg = self.rwSpeedMsgIn()
             wheelSpeeds = self.rwSpeedMsg.wheelSpeeds
             
         q_error = quat.quat_error(self.q_target, q) # get error quaternion, this function automatically sanitizes by performing normalization and hemisphere checks
+        
+        # convert error quaternions to nominal body frame, as IMU, Inertia Matrix, and Reaction Wheels are all defined in this frame already.
+        if self.pointing == "ST":
+            q_error = quat.quat_mult(self.ST2B, quat.quat_mult(q_error, self.B2ST)) # equivalent to q_error_body = R_b_st * q_error_st * R_st_b
+        elif self.pointing == "SC":
+            pass # q_error is already in the correct frame
+        elif self.pointing == "CFC":
+            q_error = quat.quat_mult(self.CFC2B, quat.quat_mult(q_error, self.B2CFC)) # equivalent to q_error_body = R_b_cfc * q_error_cfc * R_cfc_b
+        
         q_error = quat.hemi(q_error) # only apply hemisphere check once after determining error quaternion to maintain associativity across hermisphere boundaries
         self.error.append(q_error) # required for plotting after conclusion of sim
         
@@ -98,15 +102,7 @@ class FlightSoftware(sysModel.SysModel):
             desired_torque = self.quaternion_controller(q_error, omega) # compute desired 3-axis torque from controller
             # desired_torque = self.sliding_bangbang_quat(q_error, omega, currentTimeNanos) # compute desired 3-axis torque from controller
             
-            # convert torques to the reaction wheel frame based on "viewpoint"
-            if self.pointing == "ST":
-                frame_torque = quat.rotate_vec_by_quat(desired_torque, quat.axis_angle_to_quaternion([0,1,0], 90))
-            elif self.pointing == "SC":
-                frame_torque = desired_torque # SC is already in reaction wheel frame
-            elif self.pointing == "CFC":
-                frame_torque = quat.rotate_vec_by_quat(desired_torque, quat.axis_angle_to_quaternion([0,1,0], -180))
-            
-            wheel_torque = self.convert_torque_to_wheels(frame_torque) # convert desired 3-axis torque to inputs for 4 wheels
+            wheel_torque = self.convert_torque_to_wheels(desired_torque) # convert desired 3-axis torque to inputs for 4 wheels
             self.command_wheel_torques(currentTimeNanos, wheel_torque, wheelSpeeds) # Write the payload
             
         if self.output_states:
@@ -122,7 +118,6 @@ class FlightSoftware(sysModel.SysModel):
             print("Current wheel speeds: ", wheelSpeeds[:4])
             print("Body rates: ", omega)
             print("Desired torque: ", desired_torque)
-            print("Frame corrected torque: ", frame_torque)
             print("Wheel Torque: ", wheel_torque)
         
     def command_wheel_torques(self, currentTimeNanos, wheel_torque, wheelSpeeds): # send commanded torque values to reaction wheels
@@ -137,7 +132,7 @@ class FlightSoftware(sysModel.SysModel):
     
     def check_torque_vals(self, wheel_torque, rwSpeeds): # ensure torque does not exceed maxTorque and that wheel speed does not exceed maxSpeed by the beginning of next step
         
-        # wheel_torque = wheel_torque * 0.1
+        wheel_torque = wheel_torque * 0.1
         
         for i in range(len(self.torque_vals[:4])):
             projected_speed = rwSpeeds[i] + (wheel_torque[i]/self.rwInertia) * self.updateTime # predicted speed at requested torque after next time step