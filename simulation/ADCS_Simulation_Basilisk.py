dc71bcd4ef3f255d9d98e12ca6ca6f046719ceb3
mee5-PDX <mee5@pdx.edu>
2026-06-28 13:48:59 -0700

    Started to implement a basilisk helper

--- simulation/ADCS_Simulation_Basilisk.py
+++ simulation/ADCS_Simulation_Basilisk.py
@@ -15,6 +15,10 @@ bskPath = __path__[0]
 from Basilisk.utilities.supportDataTools.dataFetcher import get_path, DataFile
 
 
+# similar file of what is in oresat-simulator
+import basilisk_helpers as bsk_helpers
+
+
 # EXAMPLE FOR READING BASILISK MESSAGING SYSTEM AND ATTRIBUTES. USED FOR DOCUMENTATION, DO NOT DELETE!!!
 # message = scObject.scStateOutMsg.read()   # works before or after ExecuteSimulation()
 # print(dir(message))
@@ -50,20 +54,13 @@ def sim_main(config):
     ########################## ORBITAL ENVIRONMENT ############################
     
     # create gravitational bodies (Earth in this case, but might add moon later as well)
-    gravFactory = simIncludeGravBody.gravBodyFactory()
-    earth = gravFactory.createEarth()
-    earth.isCentralBody = True  # must be central for orbital motion
-    mu_earth = earth.mu # get gravitational object's gravitational parameter
-    gravFactory.addBodiesTo(scObject)  # automatically adds all created bodies
-    
+    gravFactory = bsk_helpers.get_gravity_factory_earth(scObject)
+    mu_earth = gravFactory.gravBodies.get("earth").mu
+
     # create the magnetic field
-    magModule = magneticFieldWMM.MagneticFieldWMM()
-    wmm_path = get_path(DataFile.MagneticFieldData.WMM)
-    magModule.configureWMMFile(str(wmm_path))
-    magModule.ModelTag = "WMM" # World Magnetic Model
-    # magModule.dataPath = bskPath + '/supportData/MagneticField/' # old version
+    magModule, magMsg, magLog = bsk_helpers.get_mag_model("WMM", scObject)
     epochMsg = unitTestSupport.timeStringToGregorianUTCMsg('2025 June 27, 10:23:0.0 (UTC)')  # set epoch date/time message for WMM
-    magModule.addSpacecraftToModel(scObject.scStateOutMsg) # add spacecraft to the magnetic field module so it can read the sc position messages
+
     sim.AddModelToTask("dynamicsTask", magModule) # add the magnetic field module to the simulation task
     magModule.epochInMsg.subscribeTo(epochMsg) # connect epoch messages
     
@@ -104,7 +101,7 @@ def sim_main(config):
     spiceObject.addPlanetNames(["earth"])
     spiceObject.zeroBase = 'Earth' # centers the spice ephemeris data on Earth. Required, otherwise WMM becomes heliocentric, and has no effect on spacecraft in Earth orbit.
     
-    earth.planetBodyInMsg.subscribeTo(spiceObject.planetStateOutMsgs[0])
+    gravFactory.gravBodies.get("earth").planetBodyInMsg.subscribeTo(spiceObject.planetStateOutMsgs[0])
     
     # Task ordering: SPICE before modules that consume its outputs. Need spice to run before spacecraft module.
     sim.AddModelToTask("dynamicsTask", spiceObject)
@@ -116,54 +113,55 @@ def sim_main(config):
     ############################### SENSORS ###################################
     
     # Create and configure a star tracker
-    starTrackerSensor = starTracker.StarTracker()
-    starTrackerSensor.ModelTag = "starTracker"
-    
     # Define dcm_CB for star tracker orientation (body-to-case, star tracker on +x side)
     dcm_CB = np.array([[0.0, 0.0, -1.0], # x_B -> -z_C
                        [0.0, 1.0, 0.0],  # y_B -> y_C
                        [1.0, 0.0, 0.0]]) # z_B -> x_C
-    starTrackerSensor.dcm_CB = dcm_CB
-    
-    starTrackerSensor.scStateInMsg.subscribeTo(scObject.scStateOutMsg)
-    starTrackerRec = starTrackerSensor.sensorOutMsg.recorder(macros.sec2nano(fsw_update_time))
-    
-    # configure Star Tracker noise
-    if config["use_filter"] == True:
-        P = np.eye(3)*config["sigma_ST"]**2
-        starTrackerSensor.PMatrix = P
-        ST_bounds = 0.5
-        starTrackerSensor.setWalkBounds([ST_bounds, ST_bounds, ST_bounds]) # bounds must be set in order to properly activate sensor noise
+
+    noise_std = config["sigma_ST"] if config["use_filter"] else None
+    w_bounds = 1 if config["use_filter"] else None
+
+    starTrackerSensor, starTrackerRec = bsk_helpers.make_star_tracker(modelTag="starTracker", 
+                                                                      scObjMsg=scObject.scStateOutMsg,
+                                                      record_t=macros.sec2nano(fsw_update_time),
+                                                      dcm_CB=dcm_CB,
+                                                      noise_std=noise_std,
+                                                      w_bounds=w_bounds)
     
     sim.AddModelToTask("fswTask", starTrackerSensor) # Add sensor to flight software task
     sim.AddModelToTask("fswTask", starTrackerRec) # Add recording to task
-    
+   
+
+
     # Create and configure IMU sensor
-    imu = imuSensor.ImuSensor()
-    imu.ModelTag = "imu"
-    imu.scStateInMsg.subscribeTo(scObject.scStateOutMsg)
-    imuRec = imu.sensorOutMsg.recorder(macros.sec2nano(fsw_update_time)) # Record the output message
-    
-    # configure IMU noise
-    if config["use_filter"] == True:
-        P = np.eye(3)*config["sigma_gyro"]**2
-        imu.PMatrixGyro = P
-        imu_bounds = 5
-        imu.setErrorBoundsGyro([imu_bounds, imu_bounds, imu_bounds]) # bounds must be set in order to properly activate sensor noise
-    
-    imu.UpdateState(0)  # Force IMU to process initial state, otherwise first value is set to zero for some reason
+    noise_std = config["sigma_gyro"] if config["use_filter"] else None
+    e_bounds = 5 if config["use_filter"] else None
+
+    imu, imuRec = bsk_helpers.make_gyro(modelTag="imu",
+                                       scObjMsg=scObject.scStateOutMsg,
+                                       record_t=macros.sec2nano(fsw_update_time),
+                                       dcm=None,
+                                       noise_std=noise_std,
+                                       e_bounds=e_bounds,
+                                       w_bounds=None)
+
     sim.AddModelToTask("fswTask", imu) # Add sensor to flight software task
     sim.AddModelToTask("fswTask", imuRec) # Add recording to task
-    
+   
+
+
     # Create magnetometer sensor
-    magSensor = magnetometer.Magnetometer()
-    magSensor.ModelTag = "TAM_sensor" # Three-Axis Magnetometer
-    magSensor.magInMsg.subscribeTo(magModule.envOutMsgs[0])
-    magSensor.stateInMsg.subscribeTo(scObject.scStateOutMsg)
-    magSensorRec = magSensor.tamDataOutMsg.recorder(macros.sec2nano(fsw_update_time))
+    magSensor, magSensorRec = bsk_helpers.make_magnetometer(modelTag="TAM_sensor",
+                                                          magMsg = magModule.envOutMsgs[0],
+                                                          scObjMsg=scObject.scStateOutMsg,
+                                                          record_t=macros.sec2nano(fsw_update_time),
+                                                          dcm=None,
+                                                          noise_std=None,
+                                                          w_bounds=None)
     sim.AddModelToTask("fswTask", magSensor)
     sim.AddModelToTask("fswTask", magSensorRec) # Add recording to task
-    
+   
+
     ############################## EFFECTORS ##################################
 
     # Create reaction wheels
@@ -202,7 +200,9 @@ def sim_main(config):
     rwStateEffector.ModelTag = "RW_cluster"
     RWFactory.addToSpacecraft(scObject.ModelTag, rwStateEffector, scObject)
     sim.AddModelToTask("dynamicsTask", rwStateEffector)
-    
+   
+
+
     # create magnetic torque bar (MTB) object
     mtbEff = MtbEffector.MtbEffector()
     mtbEff.ModelTag = "MtbEff"
@@ -262,7 +262,9 @@ def sim_main(config):
     
     # print(f"\nSatellite view device is \"{fsw.pointing}\" with initial reference: {q_init}")
     # print(f"Satellite initial pointing target: {fsw.q_target}\n")
-    
+   
+
+
     ############################## SIMULATION #################################
     # log reaction wheel behavior
     rwSpeedLog = rwStateEffector.rwSpeedOutMsg.recorder()
@@ -416,11 +418,14 @@ if __name__ == "__main__":
     
     # sigma_gyro = 0.1 * macros.D2R # instantaneous white noise (datasheet gives value in degrees, convert to radians) (not sure which to use)
     sigma_gyro = 0.014 * macros.D2R # instantaneous white noise (datasheet gives value in degrees, convert to radians) (not sure which to use)
+    # sigma_gyro = 1 * macros.D2R # instantaneous white noise (datasheet gives value in degrees, convert to radians) (not sure which to use)
     sigma_bias = 1e-5 # slow random bias drift (random walk)
     P_b0 = 1 * macros.D2R # [rad/s] initial gyro uncertainty
     
     sigma_ST = 2.4e-6 # [rad] measurement noise (instantaneous orientation error)
+    # sigma_ST = 0.01
     P_ST_0 = 8.7e-7 # [rad^2] initial star tracker attitude uncertainty
+    # P_ST_0 = 0.01
     ST_update_rate = 1.1 # defined in seconds
     
     # initial satellite states
